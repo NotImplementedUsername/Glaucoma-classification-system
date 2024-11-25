d9810b08f29c3816b77e30ef34757b93f8c00947
NotImplementedUsername <105523062+NotImplementedUsername@users.noreply.github.com>
2024-11-25 01:14:51 +0100

    Example: model training and testing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
+import torch
+from data import Eye_fundus_dataset
+from utils import get_model, save_model
+from pathlib import Path
+from torch.utils.data import DataLoader
+from sklearn.metrics import precision_score, recall_score, f1_score
+
 if __name__ == "__main__":
-    # TODO get model
+    model = get_model()
 
-    # TODO test model
+    test_dataset = Eye_fundus_dataset(Path('./data/test/negative'), Path('./data/test/positive'))
+    test_dataloader = DataLoader(test_dataset, batch_size=10, shuffle=True)
+    classification_threshold = 0.5
 
-    # TODO save model
-    
-    pass
+    model.eval()    # Model testing
+    with torch.no_grad():
+        labels = []
+        outputs = []
+        for i, data in enumerate(test_dataloader):
+            images, batch_labels = data
+            
+            labels += batch_labels.tolist()
+            batch_outputs = model(images)[:, 1]
+            batch_outputs = batch_outputs > classification_threshold
+            outputs += batch_outputs.type(torch.int32).tolist()
+            
+        precision = precision_score(labels, outputs)
+        recall = recall_score(labels, outputs)
+        f1 = f1_score(labels, outputs)
+        print(f'Model testing, Precisson: {precision}, Recall: {recall}, f1: {f1}')
+
+    save_model(model, './model.pt')
